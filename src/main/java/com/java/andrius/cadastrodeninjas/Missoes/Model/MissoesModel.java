179a270d15e3f53f38ba8a4582e4af260ab18500
andrius anselmi <andriusanselmi12@gmail.com>
2026-04-12 18:36:56 -0300

    feat: Modelagem do banco de dados NinjaModel e MissoesModel concluidos, anotations feita para relacionamento de tabelas

---
+++ src/main/java/com/java/andrius/cadastrodeninjas/Missoes/Model/MissoesModel.java
@@ -0,0 +1,48 @@
+package com.java.andrius.cadastrodeninjas.Missoes.Model;
+
+import com.java.andrius.cadastrodeninjas.Ninjas.Model.NinjaModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+    //OneToMany significa que uma missão pode ter varios ninjas(NinjaModel)
+    // mappedBY: JPA entende que é o mesmo relacionamento e usa a FK que já existe no Ninja
+    //Missão usa a mesma FK pra achar os ninjas dela
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
+
+
+    public MissoesModel() {
+    }
+
+    public MissoesModel(String nome, String dificuldade) {
+        this.nome = nome;
+        this.dificuldade = dificuldade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+}
--- src/main/java/com/java/andrius/cadastrodeninjas/Ninjas/Model/NinjaModel.java
+++ src/main/java/com/java/andrius/cadastrodeninjas/Ninjas/Model/NinjaModel.java
@@ -3,8 +3,6 @@ package com.java.andrius.cadastrodeninjas.Ninjas.Model;
 import com.java.andrius.cadastrodeninjas.Missoes.Model.MissoesModel;
 import jakarta.persistence.*;
 
-import java.util.List;
-
 //Entity transforma uma classe em uma entidade do Banco de Dados
 @Entity
 @Table(name = "tb_cadastro")
@@ -16,7 +14,16 @@ public class NinjaModel  {
     private String nome;
     private String email;
     private int idade;
-    private List<MissoesModel> missoes;
+
+
+    //No código, o @ManyToOne dentro da classe NinjaModel, está dizendo ao
+    // banco de dados: "Muitos objetos desta classe (Ninja) podem apontar para um
+    // único objeto da classe referenciada (MissoesModel)."
+    @ManyToOne
+    //A gente criar uma Foreign Key(Chave Estrangeira) para que a tabela ninja
+    //possa enxergar a tabela de missoes
+    @JoinColumn(name = "missoes_id")
+    private MissoesModel missoes;
 
     public NinjaModel() {
     }
