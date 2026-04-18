b3da26ad0d6c1205cef2f8b8138a6f996409e045
andrius anselmi <andriusanselmi12@gmail.com>
2026-04-17 23:17:18 -0300

    feat: Cria o mock de endpoint da classe Ninja

--- src/main/java/com/java/andrius/cadastrodeninjas/Ninjas/NinjaController.java
+++ src/main/java/com/java/andrius/cadastrodeninjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package com.java.andrius.cadastrodeninjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,36 @@ public class NinjaController {
     @GetMapping("/boasVindas")
     public String boasVindas(){
         return "Essa é a minha primeira mensagem nessa rota";
+    }
+
+    //Adiconar Ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return  "Ninja criado com sucesso";
+    }
+
+    //Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return  "Mostrando todos os ninjas";
+    }
+
+    //Mostrar Ninja por ID (READ)
+    @GetMapping("todosID")
+    public String mostrarPorId(int path){
+        return  "Mostrando ninja por id";
+    }
+
+
+    //Altera dados dos Ninja (UPDATE)
+    @PutMapping("alterarID")
+    public String alterarPorId(){
+        return  "Alterando ninja por ID";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("deletarID")
+    public String deletarPorId(){
+        return  "Deletando ninja pro ID";
     }
 }
