b9c2d11436a282a71da0fbdc284d5d66f6d990ca
andrius anselmi <andriusanselmi12@gmail.com>
2026-04-18 11:28:57 -0300

    feat: Cria as rotas do controller com mock para classe Missoes

--- src/main/java/com/java/andrius/cadastrodeninjas/Missoes/MissoesController.java
+++ src/main/java/com/java/andrius/cadastrodeninjas/Missoes/MissoesController.java
@@ -1,4 +1,34 @@
 package com.java.andrius.cadastrodeninjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return  "Missão criada";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarTodasAsMissoes(){
+        return  "Mostrando missões";
+    }
+
+    @GetMapping("/mostrarID")
+    public String mostrarPorId(){
+        return "Mostrando missão por id";
+    }
+
+    @PutMapping("/alterarID")
+    public String alterarPorId(){
+        return "Missão alterado com sucesso";
+        }
+
+    @DeleteMapping("/deletarID")
+    public String deletarPorId(){
+        return "Missão deltado com sucesso";
+    }
 }
+
