662146b39cb99fbcf2bbb4af378b7da40d2b0f10
andrius anselmi <andriusanselmi12@gmail.com>
2026-04-24 21:01:42 -0300

    feat: Inclui DTO e Mapper para ser usado posteriormente na camada Service e Controller

---
+++ src/main/java/com/java/andrius/cadastrodeninjas/Missoes/MissoesMapper.java
@@ -0,0 +1,25 @@
+package com.java.andrius.cadastrodeninjas.Missoes;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MissoesMapper {
+
+    public MissoesModel mapDTO(MissoesDTO missoesDTO){
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO mapModel(MissoesModel missoesModel){
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNome(missoesModel.getNome());
+        missoesDTO.setId(missoesModel.getId());
+
+        return missoesDTO;
+    }
+}
